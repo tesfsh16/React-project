87d3611f297a452de62ac3b9d889fd3a0507e5a7
tesfsh16 <ayenewmakbel@gmail.com>
2025-12-31 06:15:49 +0300

    completed to do is finished

--- src/App.jsx
+++ src/App.jsx
@@ -28,15 +28,48 @@ function App() {
       )
     );
   };
-const deleteTodo = (id) => {
-  setTodos(todos.filter((todo) => todo.id !== id));
-};
+
+  // Delete todo
+  const deleteTodo = (id) => {
+    setTodos(todos.filter((todo) => todo.id !== id));
+  };
 
   
 
-  return 
-    
-  
-};
+  return (
+    <div>
+      <h2>Todo List</h2>
+
+      <div>
+        <input
+          type="text"
+          value={input}
+          placeholder="Enter a task"
+          onChange={(e) => setInput(e.target.value)}
+        />
+        <button onClick={addTodo}>Add</button>
+      </div>
+
+      <ul>
+        {todos.map((todo) => (
+          <li key={todo.id}>
+            <span onClick={() => toggleTodo(todo.id)}>{todo.text}</span>
+            <button
+              onClick={() => toggleTodo(todo.id)}
+              style={{
+                color: todo.completed ? "red" : "black",
+              }}
+            >
+              completed
+            </button>
+            <button onClick={() => deleteTodo(todo.id)}>❌</button>
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
+}
+
+
 
 export default App;
